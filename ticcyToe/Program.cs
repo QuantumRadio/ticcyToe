62cfaba6ae8c9c902bbb26d5894985560d5b626c
elalimardani <58517842+elalimardani@users.noreply.github.com>
2019-12-09 10:02:34 -0500

    Update Program.cs

--- ticcyToe/Program.cs
+++ ticcyToe/Program.cs
@@ -4,6 +4,9 @@ namespace ticcyToe
 {
     class Program
     {
+        
+        string array[] = {'1' , '2', '3', '4', '5', '6', '7', '8', '9'};
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
