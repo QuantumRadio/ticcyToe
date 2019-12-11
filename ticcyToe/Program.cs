c9e9ca45a8e0934150b01a03a5ae9c52ee5aee58
elalimardani <58517842+elalimardani@users.noreply.github.com>
2019-12-11 08:49:08 -0500

    Update Program.cs

--- ticcyToe/Program.cs
+++ ticcyToe/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 
 namespace ticcyToe
 {
     class Program
     {
         
-         static void drawBoard(string[] args){
+         //draw board method
+         static void DrawBoard(string[] args){
            
            char[,] board = new char [3,3];
            board[0,0]= 'X';
@@ -22,8 +23,62 @@ namespace ticcyToe
            }
         }
 
-        
-        
-        
+        //////check win methods
+
+        //HorWin
+        //win = true;
+
+        //VerWin
+        //win =true;
+
+        //DiWin
+        //win = true;
+
+
+        //main method
+        static void Main(string[] args){
+
+            Console.WriteLine("You will play a game of ticcy toe with a partner. X goes first");
+
+            bool win=false;
+
+            if(win==false){
+
+                //keep playing
+                 for(int i=0; i<10; i++){
+                    //ask the player for spot
+                    Console.WriteLine("Which spot would you like to mark?");
+                    string chosenSpot = Console.ReadLine();
+
+                    //user_proof
+                    if(chosenSpot != "1" || chosenSpot!= "2"||chosenSpot!="3"||chosenSpot!="4"||chosenSpot!="5"||chosenSpot!="6"||chosenSpot!="7"||chosenSpot!="8"||chosenSpot!="9"){
+                   Console.WriteLine("Please choose only from numbers 1 to 9");
+                   }
+
+                    //check if spot is not taken
+
+                }
+
+            }
+            
+           
+            /* string array[] = {'1' , '2', '3', '4', '5', '6', '7', '8', '9'};
+            string[] xOrO = [O,X]; */
+
+            drawBoard(args);
+
+            /* for (int i=0; i<10; i++)
+            {
+                string temp = xOrO[0];
+                xOrO[0] = xOrO[1];
+                xOrO [1] = temp;
+
+                Console.Writeline("Which spot would you like to mark, {0}?",xOrO[0]);
+                int chosenSpot= 0;
+                chosenSpot = Console.ReadLine();
+                array[chosenSpot] = xOrO;
+            } */
+
+        }
     }
 }
