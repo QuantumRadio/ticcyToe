334094ec5b0b7c4c1fb369d9396b15ef9d7f6f39
QuantumRadio <57708208+QuantumRadio@users.noreply.github.com>
2019-12-19 09:22:04 -0500

    who fucking knows
    None of this code makes any fucking sense

--- ticcyToe/Program.cs
+++ ticcyToe/Program.cs
@@ -6,28 +6,28 @@ namespace ticcyToe
     {
         
         //draw board method
-        static void DrawBoard(){  
-           char[,] drawBoard = new char[3,3];
-           drawBoard[0,0] = '0';
-           drawBoard[0,1] = '1';
-           drawBoard[0,2] = '2';
-           drawBoard[1,0] = '3';
-           drawBoard[1,1] = '4';
-           drawBoard[1,2] = '5';
-           drawBoard[2,0] = '6';
-           drawBoard[2,1] = '7';
-           drawBoard[2,2] = '8';
+        static void DrawBoard(string[] board){  
+            string[,] drawBoard = new string[3,3];
+            drawBoard[0,0] = board[0];
+            drawBoard[0,1] = board[1];
+            drawBoard[0,2] = board[2];
+            drawBoard[1,0] = board[3];
+            drawBoard[1,1] = board[4];
+            drawBoard[1,2] = board[5];
+            drawBoard[2,0] = board[6];
+            drawBoard[2,1] = board[7];
+            drawBoard[2,2] = board[8];
 
-           for(int row=0; row<3; row++)
-           {
-               Console.Write("| ");
-               for(int col= 0; col<3; col++)
-               {
+            for(int row=0; row<3; row++)
+            {
+                Console.Write("| ");
+                for(int col= 0; col<3; col++)
+                {
                    Console.Write(drawBoard[row, col]);
                    Console.Write(" | ");
-               }
-               Console.WriteLine();
-           }
+                }
+                Console.WriteLine();
+            }
         }
 
         //check spot
@@ -43,48 +43,57 @@ namespace ticcyToe
         }
 
         //check win methods
-        /*static void Wins(char[] drawBoard, string[] chosenSpot)
+        static void Wins(string[] board, string[] xOrO)
         {
         
-        if (drawBoard[0,0] == drawBoard[1,1] == drawBoard[1,2] ||
-            drawBoard[1,0] == drawBoard[1,1] == drawBoard[1,2] ||
-            drawBoard[2,0] == drawBoard[2,1] == drawBoard[2,2])
+        //horizontal win
+        if ((String.Equals(board[0], board[1]) && String.Equals(board[1], board[2])) ||
+            (String.Equals(board[3], board[4]) && String.Equals(board[4], board[5])) ||
+            (String.Equals(board[6], board[7]) && String.Equals(board[7], board[8])))
             {
-                
+                Console.WriteLine("Congratulations! Player {0} wins!", xOrO[1]); 
             }
 
 
-        else if (drawBoard[0,0] == drawBoard[0,1] == drawBoard[0,2] ||
-                 drawBoard[1,0] == drawBoard[1,1] == drawBoard[1,2] ||
-                 drawBoard[2,0] == drawBoard[2,1] == drawBoard[2,2])
+        //vertical win
+        else if ((String.Equals(board[0], board[3]) && String.Equals(board[3], board[6])) ||
+                 (String.Equals(board[1], board[4]) && String.Equals(board[4], board[7])) ||
+                 (String.Equals(board[2], board[5]) && String.Equals(board[5], board[8])))
             {
-
+                Console.WriteLine("Congratulations! Player {0} wins!", xOrO[1]); 
             }
 
-        else if (drawBoard[0,0] == drawBoard[1,1] == drawBoard[2,2] ||
-                 drawBoard[0,2] == drawBoard[1,1] == drawBoard[2,0])
+        //diagonal win
+        else if ((String.Equals(board[0], board[4]) && String.Equals(board[4], board[8])) ||
+                 (String.Equals(board[2], board[4]) && String.Equals(board[4], board[6])))
             {
-
+                Console.WriteLine("Congratulations! Player {0} wins!", xOrO[1]); 
             }
-        }*/
+        }
+        
         static void Main(string[] args){
 
             //intro
             Console.WriteLine("You will play a game of ticcy toe with a partner. X goes first");
             
             //defining things
-            string[] board = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            string[] board = new string[9];
+            for (int i=0; i<9;i++)
+            {
+                board[i] = Convert.ToString(i+1);
+            }
             string[] xOrO = {"O","X"};
             
 
             //turns
             for (int i=0; i<9; i++){
+
+                Wins();
                 string temp = xOrO[0];
                 xOrO[0] = xOrO[1];
                 xOrO[1] = temp;
 
                 DrawBoard();
-                //Wins();
             
                 //take spot
                 Console.WriteLine("Which spot would you like to mark, {0}?", xOrO[0]);
